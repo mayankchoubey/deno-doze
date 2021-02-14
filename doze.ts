af77a477c5ccfc55d8cd4bb2c95c1e86ab68d3f7
Mayank Choubey <mayank.choubey@ushur.com>
2021-02-13 23:21:35 -0800

    .

--- doze.ts
+++ doze.ts
@@ -1,15 +1,33 @@
-//Supported functions
-// -- Doze : seconds
-// -- DozeMS: miliseconds
-
+/**
+ * Asynchronously sleep for given seconds.
+ * @param {number} seconds - The seconds for which to sleep asynchronously.
+ * @returns {Promise}
+ * @example
+ * await doze(1);
+ */
 export function doze(seconds: number) {
     return dozeInternal(seconds*1000);
 }
 
+/**
+ * Asynchronously sleep for given milliseconds.
+ * @param {number} milliseconds - The milliseconds for which to sleep asynchronously.
+ * @returns {Promise}
+ * @example
+ * await dozeMS(100);
+ */
 export function dozeMS(mseconds: number) {
     return dozeInternal(mseconds);
 }
 
+/**
+ * Asynchronously sleep for given milliseconds.
+ * @private
+ * @param {number} milliseconds - The milliseconds for which to sleep asynchronously.
+ * @returns {Promise}
+ * @example
+ * await dozeInternal(100);
+ */
 function dozeInternal(msecs: number) {
     return new Promise(resolve => setTimeout(resolve, msecs));
 }
